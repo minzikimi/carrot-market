0449b66554577215242353f0b4ad111da0643d63
minzikimi <minji.kim@hyperisland.se>
2025-04-18 19:56:29 +0200

    server action

--- components/form-btn.tsx
+++ components/form-btn.tsx
@@ -1,15 +1,22 @@
+"use client";
+
+import { useFormStatus } from "react-dom";
+
 interface FormButtonProps {
-    loading: boolean;
+    // loading: boolean;
     text: string;
   }
   
-  export default function FormButton({ loading, text }: FormButtonProps) {
+  export default function FormButton({ text }: FormButtonProps) {
+    const { pending } = useFormStatus(); //this hook only can be used in child of form, that needs to be modified.
     return (
       <button
-        disabled={loading}
+        disabled={pending}
         className="primary-btn h-10 disabled:bg-neutral-400  disabled:text-neutral-300 disabled:cursor-not-allowed"
       >
-        {loading ? "로딩 중" : text}
+        {pending ? "로딩 중" : text}
       </button>
     );
   }
+
+  //make server action, and  put action in form component
