aa3b5fe68bb4854ca2d5dec5b13d824bc77c68e2
minzikimi <minji.kim@hyperisland.se>
2025-04-27 23:22:14 +0200

    pagination actions

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -1,47 +1,66 @@
 "use client";
- 
- import { InitialProducts } from "@/app/(tabs)/products/page";
- import ListProduct from "./list-product";
- import { useState } from "react";
- import { getMoreProducts } from "@/app/(tabs)/products/actions";
- 
- interface ProductListProps {
-   initialProducts: InitialProducts;
- }
- 
- export default function ProductList({ initialProducts }: ProductListProps) {
-   const [products, setProducts] = useState(initialProducts);
-   const [isLoading, setIsLoading] = useState(false);
-   const [page, setPage] = useState(0);
-   const [isLastPage, setIsLastPage] = useState(false);
-   
-   const onLoadMoreClick = async () => {
-     setIsLoading(true);
-     const newProducts = await getMoreProducts(page + 1);
-     if (newProducts.length !== 0) {
-       setPage((prev) => prev + 1);
-       setProducts((prev) => [...prev, ...newProducts]);
-     } else {
-       setIsLastPage(true);
-     }
-     setIsLoading(false);
-   };
-   return (
-     <div className="p-5 flex flex-col gap-5">
-       {products.map((product) => (
-         <ListProduct key={product.id} {...product} />
-       ))}
-        {isLastPage ? (
-         "No more items"
-       ) : (
-         <button
-           onClick={onLoadMoreClick}
-           disabled={isLoading}
-           className="text-sm font-semibold bg-orange-500 w-fit mx-auto px-3 py-2 rounded-md hover:opacity-90 active:scale-95"
-         >
-           {isLoading ? "로딩 중" : "Load more"}
-         </button>
-       )}
-     </div>
-   );
- }
+
+import { InitialProducts } from "@/app/(tabs)/products/page";
+import ListProduct from "./list-product";
+import { useEffect, useRef, useState } from "react";
+import { getMoreProducts } from "@/app/(tabs)/products/actions";
+
+interface ProductListProps {
+  initialProducts: InitialProducts;
+}
+
+export default function ProductList({ initialProducts }: ProductListProps) {
+  const [products, setProducts] = useState(initialProducts);
+  const [isLoading, setIsLoading] = useState(false);
+  const [page, setPage] = useState(0);
+  const [isLastPage, setIsLastPage] = useState(false);
+  const trigger = useRef<HTMLSpanElement>(null);
+
+  useEffect(() => {
+    if (isLastPage) return; // Don't observe if last page
+
+    const observer = new IntersectionObserver(
+      async (entries, observer) => {
+        const element = entries[0];
+        if (element.isIntersecting && trigger.current) {
+          observer.unobserve(trigger.current);
+          setIsLoading(true);
+          const newProducts = await getMoreProducts(page + 1);
+          if (newProducts.length !== 0) {
+            setPage((prev) => prev + 1);
+            setProducts((prev) => [...prev, ...newProducts]);
+          } else {
+            setIsLastPage(true);
+          }
+          setIsLoading(false);
+        }
+      },
+      { threshold: 1.0 }
+    );
+
+    if (trigger.current) {
+      observer.observe(trigger.current);
+    }
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [page, isLastPage]);
+
+  return (
+    <div className="p-5 flex flex-col gap-5">
+      {products.map((product) => (
+        <ListProduct key={product.id} {...product} />
+      ))}
+
+      {!isLastPage && (
+        <span
+          ref={trigger}
+          className="mt-[300vh] mb-96 text-sm font-semibold bg-orange-500 w-fit mx-auto px-3 py-2 rounded-md hover:opacity-90 active:scale-95"
+        >
+          {isLoading ? "로딩 중" : "Load more"}
+        </span>
+      )}
+    </div>
+  );
+}
